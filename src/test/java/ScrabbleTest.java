e73f7d6a721216c330667f8ffcc5dfbd4996b3d0
Farnoosh Johnson and Nick Embrey <cne.embrey@gmail.com>
2016-04-19 09:58:58 -0700

    Makes returnsScoreForAllLettersOfSomeValue fail

--- src/test/java/ScrabbleTest.java
+++ src/test/java/ScrabbleTest.java
@@ -15,6 +15,13 @@ import static org.junit.Assert.*;
 
  //The Scrabble score take all the letters that are valued the same number and returns a Scrabble score for the same number multiple by the number od letters.For example we expect an input of AEIOULNRST to return 10.
 
+ @Test
+ public void runScrabble_returnsScoreforAllLettersOfSomeValue_10() {
+   Scrabble testScrabble = new Scrabble();
+   Integer expected = 10;
+   assertEquals(expected, testScrabble.runScrabble("AEIOULNRST"));
+ }
+
  //The Scrabble score take a word and returns a Scrabble score for that word.For example we expect an input of ACE to return 5.
 
  //The Scrabble score take a letter and returns a Scrabble score for that letter regardless of case.For example we expect an input of a to return 1.
